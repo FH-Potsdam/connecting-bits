ee64f8dbeb8058bb39eabede2dfed0bc4c732c4c
Lucas Vogel <contact@vogelino.ch>
2016-01-17 13:30:48 +0100

    Very basic shiftr setup

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,10 @@
 import config from 'config';
 import { Boards, Led } from 'johnny-five';
 import Particle from 'particle-io';
+import { connect } from 'mqtt';
+
+const { usr, psw, clientId } = config.get('shiftr');
+const client = connect(`mqtt://${usr}:${psw}@broker.shiftr.io`, { clientId });
 
 const boards = new Boards([
 	{
@@ -38,4 +42,19 @@ boards.on('ready', function() {
 		const led = new Led({ pin: 'D7', board });
 		led.stop().blink();
 	});
+});
+
+client.on('connect', function(){
+	console.log('client has connected!');
+
+	client.subscribe('/example');
+	// client.unsubscribe('/example');
+
+	setInterval(function(){
+		client.publish('/hello', 'world');
+	}, 1000);
+});
+
+client.on('message', function(topic, message) {
+	console.log('new message:', topic, message.toString());
 });
