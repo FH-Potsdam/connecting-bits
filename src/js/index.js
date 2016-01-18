800849662993b54ec94fb63269e3b365f4424c5b
Lucas Vogel <contact@vogelino.ch>
2016-01-18 20:49:44 +0100

    Acheived a ping pong communication with shiftr between two boards

--- src/js/index.js
+++ src/js/index.js
@@ -40,14 +40,19 @@ const vogelitonClient = getClientByName('vogeliton');
 const thonelinoBoard = getBoardByName('thonelino');
 const thonelinoClient = getClientByName('thonelino');
 
+let vogelitonLed;
+let thonelinoLed;
+
 let vogelitonInterval;
 vogelitonClient.client.on('message', (topic, message) => {
 	clearTimeout(vogelitonInterval);
 	console.log(`vogeliton received a message: "${message.toString()}" on topic: ${topic}`);
 
+	thonelinoLed.blink();
 	vogelitonInterval = setTimeout(() => {
 		vogelitonClient.client.publish('/inputs/thonelino', 'from vogeliton to thonelino');
-	}, 1000);
+		thonelinoLed.off();
+	}, 3000);
 });
 
 let thonelinoInterval;
@@ -55,18 +60,24 @@ thonelinoClient.client.on('message', (topic, message) => {
 	clearTimeout(thonelinoInterval);
 	console.log(`thonelino received a message: "${message.toString()}" on topic: ${topic}`);
 
+	thonelinoLed.blink();
 	thonelinoInterval = setTimeout(() => {
 		thonelinoClient.client.publish('/inputs/vogeliton', 'from thonelino to vogeliton');
-	}, 1000);
+		thonelinoLed.off();
+	}, 3000);
 });
 
 vogelitonClient.client.on('connect', () => {
 	vogelitonClient.client.subscribe('/inputs/vogeliton');
 	vogelitonBoard.on('ready', () => {
+		vogelitonLed = new Led({ pin: 'D7', board: vogelitonBoard });
 		vogelitonClient.client.publish('/inputs/thonelino','from vogeliton to thonelino');
 	});
 });
 
 thonelinoClient.client.on('connect', () => {
 	thonelinoClient.client.subscribe('/inputs/thonelino');
+	thonelinoBoard.on('ready', () => {
+		thonelinoLed = new Led({ pin: 'D7', board: thonelinoBoard });
+	});
 });
