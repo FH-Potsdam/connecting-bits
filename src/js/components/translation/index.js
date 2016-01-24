ca5aac75ba52ed51f045368fc7bb128f74e949bd
Ourelius <andre@rockin-mustache.de>
2016-01-24 10:01:07 +0100

    rewriting record and report classes

--- src/js/components/translation/index.js
+++ src/js/components/translation/index.js
@@ -8,7 +8,7 @@ const request = require('request');
 const terminal = require('oh-my-terminal');
 
 import Record from './utils/records';
-import Report from './utils/reports';
+import CreateReport from './utils/reports';
 
 const clientId = 'test-app'; // Can be anything
 
@@ -21,35 +21,67 @@ let sensorStatus        = config.get('Dev.sensor');
 
 
 
+
+function reportAudio(index) {
+  let location       = config.get('Report.location');
+  let filename       = config.get('Report.filename');
+  let audioId        = index;
+  let recordingDate  = new Date();
+
+  let data = {
+    filename: config.get('Audio.filename'),
+    id: audioId,
+    location: config.get('Audio.location'),
+    samplerate: config.get('Audio.sampleRate'),
+    maxRecordTime: config.get('Audio.maxRecordingTime'),
+    recordingDate: recordingDate,
+  };
+
+  let newReport = new CreateReport(location, filename, data);
+  if (!fs.existsSync( location + filename  )) {
+    newReport.writeJSON();
+  } else {
+    newReport.extendJSON();
+  }
+}
+
+
+
+// execute script
 if( sensorStatus == 1 ) {
 
   function startRecording(callback){
-    let JSONlocation       = config.get('Report.location');
-    let JSONfilename       = config.get('Report.filename');
-    let index              = 0;
+    let audioReportLocation = config.get('Report.location');
+    let audioReportFilename = config.get('Report.filename');
+    let index               = 0;
 
-    if (fs.existsSync( JSONlocation + JSONfilename )) {
-      let contents = fs.readFileSync( JSONlocation + JSONfilename );
+    if (fs.existsSync( audioReportLocation + audioReportFilename )) {
+      let contents = fs.readFileSync( audioReportLocation + audioReportFilename );
       let jsonContent = JSON.parse(contents);
 
-      for( index = 0; index < jsonContent.records.length; index++ ) {
-        let recording_id = jsonContent.records[index].recording_id;
-        index = recording_id;
+      for( index = 0; index < Object.keys(jsonContent.results).length; index++ ) {
+        index = index;
       }
     }
 
-    let startRecord = new Record( 'german', index, () => {
-        console.log('recording done');
-        console.log('soundfile written');
+    // init array
+    let recordingOptions = {
+      language: 'german',
+      id: index,
+      location: config.get('Audio.location'),
+      filename: config.get('Audio.filename'),
+      sampleRate: config.get('Audio.sampleRate'),
+      maxRecTime: config.get('Audio.maxRecordingTime')
+    };
 
-        let report = new Report(index);
-    });
+    let startRecord = new Record( recordingOptions, reportAudio(index));
   }
 
+  console.log('recording starts after 5 seconds ... ');
+  setTimeout( () => {
+    startRecording();
+  }, 5000); // stop after 10 seconds
 
-  setTimeout(startRecording( () => {
-    // speech to text
-  }), 30000); // loop each 30 seconds
 
 } else {
 
--- src/js/components/translation/utils/reports/index.js
+++ src/js/components/translation/utils/reports/index.js
@@ -1,66 +1,31 @@
 const config = require('config');
 const fs = require('fs');
 
-export default class Report {
-  constructor(index){
 
-    this.location       = config.get('Report.location');
-    this.filename       = config.get('Report.filename');
+export default class CreateReport {
+  constructor(location, filename, data) {
 
-    this.audioId        = index;
+    this.data = data;
+    this.location       = location;
+    this.filename       = filename;
 
-    this.audioLocation  = config.get('Audio.location');
-    this.audioFilename  = config.get('Audio.filename');
-    this.sampleRate     = config.get('Audio.sampleRate');
-    this.maxRecTime     = config.get('Audio.maxRecordingTime');
-    this.recordingDate  = new Date();
+    this.newObject = { results: [] };
+    this.newObject.results.push(data);
 
-    this.prepareJSON();
-
+    console.log('init object');
   }
 
-  prepareJSON() {
-    console.log('peparing report file');
-
-    let recordStorage = {
-      records: [{
-        filename: this.audioFilename + '_' + this.audioId + '.wav',
-        location: this.audiolocation,
-        recording_id: this.audioId.toString(),
-        //languageInput: this.language,
-        sampleRate: this.sampleRate.toString(),
-        timestamp: this.recordingDate
-      }]
-    };
-
-    this.writeJSON(recordStorage)
+  writeJSON() {
+    fs.writeFileSync( this.location + this.filename , JSON.stringify( this.newObject ));
+    console.log('new JSON-file written');
   }
 
-  writeJSON(recordStorage) {
-    if (!fs.existsSync( this.location + this.filename  )) {
-      fs.writeFileSync( this.location + this.filename , JSON.stringify( recordStorage ));
-      console.log('new JSON-file written');
-    } else {
-      // Get content from file
-      let contents = fs.readFileSync( this.location + this.filename  );
-      // Define to JSON type
-      let jsonContent = JSON.parse(contents);
+  extendJSON() {
+    let contents = fs.readFileSync( this.location + this.filename  );
+    let jsonContent = JSON.parse(contents);
 
-      // pushing new record to json
-      jsonContent['records'].push(
-        {
-          filename: this.audioFilename + '_' + '.wav',
-          location: this.audiolocation,
-          recording_id: this.audioId.toString(),
-          //languageInput: this.language,
-          sampleRate: this.sampleRate.toString(),
-          timestamp: this.recordingDate
-        }
-      );
-      // Write file
-      fs.writeFileSync( this.location + this.filename, JSON.stringify(jsonContent) );
-      console.log('JSON-file extended');
-      console.log(jsonContent);
-    }
+    jsonContent['results'].push(this.data);
+    fs.writeFileSync( this.location + this.filename, JSON.stringify(jsonContent) );
+    console.log('JSON-file extended');
   }
 }
