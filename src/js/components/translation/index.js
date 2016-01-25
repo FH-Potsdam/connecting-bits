feaf7c4bb39a4667080499c94f9ab8b0d5bd6c88
Lucas Vogel <contact@vogelino.ch>
2016-01-25 13:27:22 +0100

    Formatted tab spaces

--- src/js/components/translation/index.js
+++ src/js/components/translation/index.js
@@ -16,33 +16,33 @@ let locTxt = 'text.txt'; // set where the test will be saved
 
 
 const googleKey         = config.get('APIKeys.google'),
-      azureKey          = config.get('APIKeys.azure');
+			azureKey          = config.get('APIKeys.azure');
 let sensorStatus        = config.get('Dev.sensor');
 
 
 
 
 function reportAudio(index) {
-  let location       = config.get('Report.location');
-  let filename       = config.get('Report.filename');
-  let audioId        = index;
-  let recordingDate  = new Date();
+	let location       = config.get('Report.location');
+	let filename       = config.get('Report.filename');
+	let audioId        = index;
+	let recordingDate  = new Date();
 
-  let data = {
-    filename: config.get('Audio.filename'),
-    id: audioId,
-    location: config.get('Audio.location'),
-    samplerate: config.get('Audio.sampleRate'),
-    maxRecordTime: config.get('Audio.maxRecordingTime'),
-    recordingDate: recordingDate,
-  };
+	let data = {
+		filename: config.get('Audio.filename'),
+		id: audioId,
+		location: config.get('Audio.location'),
+		samplerate: config.get('Audio.sampleRate'),
+		maxRecordTime: config.get('Audio.maxRecordingTime'),
+		recordingDate: recordingDate,
+	};
 
-  let newReport = new CreateReport(location, filename, data);
-  if (!fs.existsSync( location + filename  )) {
-    newReport.writeJSON();
-  } else {
-    newReport.extendJSON();
-  }
+	let newReport = new CreateReport(location, filename, data);
+	if (!fs.existsSync( location + filename  )) {
+		newReport.writeJSON();
+	} else {
+		newReport.extendJSON();
+	}
 }
 
 
@@ -50,41 +50,41 @@ function reportAudio(index) {
 // execute script
 if( sensorStatus == 1 ) {
 
-  function startRecording(callback){
-    let audioReportLocation = config.get('Report.location');
-    let audioReportFilename = config.get('Report.filename');
-    let index               = 0;
+	function startRecording(callback){
+		let audioReportLocation = config.get('Report.location');
+		let audioReportFilename = config.get('Report.filename');
+		let index               = 0;
 
-    if (fs.existsSync( audioReportLocation + audioReportFilename )) {
-      let contents = fs.readFileSync( audioReportLocation + audioReportFilename );
-      let jsonContent = JSON.parse(contents);
+		if (fs.existsSync( audioReportLocation + audioReportFilename )) {
+			let contents = fs.readFileSync( audioReportLocation + audioReportFilename );
+			let jsonContent = JSON.parse(contents);
 
-      for( index = 0; index < Object.keys(jsonContent.results).length; index++ ) {
-        index = index;
-      }
-    }
+			for( index = 0; index < Object.keys(jsonContent.results).length; index++ ) {
+				index = index;
+			}
+		}
 
-    // init array
-    let recordingOptions = {
-      language: 'german',
-      id: index,
-      location: config.get('Audio.location'),
-      filename: config.get('Audio.filename'),
-      sampleRate: config.get('Audio.sampleRate'),
-      maxRecTime: config.get('Audio.maxRecordingTime')
-    };
+		// init array
+		let recordingOptions = {
+			language: 'german',
+			id: index,
+			location: config.get('Audio.location'),
+			filename: config.get('Audio.filename'),
+			sampleRate: config.get('Audio.sampleRate'),
+			maxRecTime: config.get('Audio.maxRecordingTime')
+		};
 
-    let startRecord = new Record( recordingOptions, reportAudio(index));
-  }
+		let startRecord = new Record( recordingOptions, reportAudio(index));
+	}
 
-  console.log('recording starts after 5 seconds ... ');
-  setTimeout( () => {
-    startRecording();
-  }, 5000); // stop after 10 seconds
+	console.log('recording starts after 5 seconds ... ');
+	setTimeout( () => {
+		startRecording();
+	}, 5000); // stop after 10 seconds
 
 
 } else {
 
-  console.log('sensor not active');
+	console.log('sensor not active');
 
 }
--- src/js/components/translation/utils/records/index.js
+++ src/js/components/translation/utils/records/index.js
@@ -1,17 +1,16 @@
-const config = require('config');
 const fs = require('fs');
 const rec = require('node-record-lpcm16');
 
 export default class Record {
-  constructor( options, callback ) {
+	constructor( options, callback ) {
 
-    let file = fs.createWriteStream(options.location + options.filename + options.id + '.wav', { encoding: 'binary' });
-    rec.start({ sampleRate: options.sampleRate, verbose: true }).pipe(file);
+		let file = fs.createWriteStream(options.location + options.filename + options.id + '.wav', { encoding: 'binary' });
+		rec.start({ sampleRate: options.sampleRate, verbose: true }).pipe(file);
 
-    setTimeout(function () {
-      rec.stop();
-      callback();
-    }, options.maxRecTime); // stop after 10 seconds
+		setTimeout(() => {
+			rec.stop();
+			callback();
+		}, options.maxRecTime); // stop after 10 seconds
 
-  }
+	}
 }
