40b9d6ba4321b8f543c883da8cc7ce585d6c8654
Lucas Vogel <contact@vogelino.ch>
2016-01-27 21:00:44 +0100

    #60 Integrated code for the servos with dummy values (not tested)

--- src/js/components/motor/index.js
+++ src/js/components/motor/index.js
@@ -1,18 +1,40 @@
+import { Servo } from 'johnny-five';
+
+const SPEED = 200;
+const LYING = 0;
+const STANING = 0;
+const TILTED = 0;
+const STRAIGHT = 0;
 
 export default class Motor {
 	constructor(board) {
-		this.board = board;
+		this.liftServo = new Servo({ pin: 'D2', board });
+		this.tiltServo = new Servo({ pin: 'D3', board });
+
+		this.stands = false;
+		this.titls = false;
+	}
+	checkAndMove(tocheck, checkValue, position) {
+		return new Promise((resolve) => {
+			if (tocheck === checkValue) {
+				resolve();
+				return;
+			}
+			this.liftServo('move:complete', resolve);
+			this.liftServo.to(position, SPEED);
+			this.liftServo('move:complete', false);
+		});
 	}
 	lieDown() {
-		return new Promise((resolve) => resolve());
+		return this.checkAndMove(this.stands, false, LYING);
 	}
 	standUp() {
-		return new Promise((resolve) => resolve());
+		return this.checkAndMove(this.stands, true, STANDING);
 	}
 	lookUp() {
-		return new Promise((resolve) => resolve());
+		return this.checkAndMove(this.titls, true, TILTED);
 	}
 	lookStraight() {
-		return new Promise((resolve) => resolve());
+		return this.checkAndMove(this.thils, false, STRAIGHT);
 	}
 }
