ed9dcbc273be38436f6be680327005b093ba30f8
Lucas Vogel <contact@vogelino.ch>
2016-02-01 18:42:20 +0100

    Integrated microphone and speaker (translator missing)

--- src/js/components/translator/index.js
+++ src/js/components/translator/index.js
@@ -12,34 +12,26 @@ export default class Translate {
 			const { location, filename } = config.get('Convert');
 			const filePath = location + filename;
 			const contents = fs.readFileSync(filePath);
-			const jsonContent = JSON.parse(contents);
-			const { language, output } = jsonContent;
-			jsonContent.filePath = filePath;
-			if (language.toLowercase() === 'deutsch') {
-				this.translate.bind(this)('de', 'en', output)
-					.then(resolve)
-					.catch(reject);
-			}
+			const texts = JSON.parse(contents).texts;
+
+			this.translate.bind(this)(this.language, 'fr', texts[this.name].output)
+				.then(resolve)
+				.catch(reject);
 		});
 	}
 	translate(sourceLanguage, targetLanguage, output) {
 		return new Promise((resolve, reject) => {
 			const api_key = config.get('APIKeys.google');
-			return fetch(`https://www.googleapis.com/language/translate
+			fetch(`https://www.googleapis.com/language/translate
 				/v2?key=${this.api_key}&q=${output}
 				&source=${sourceLanguage}&target=${targetLanguage}`.trim())
 				.then(res => res.json())
 				.then(jsonContent => {
-					const { location, filename } = config.get('Convert');
-					const filePath = location + filename;
-					const content = JSON.stringify({ language: targetLanguage, output });
-					fs.writeFileSync(filePath, content, (writeError) => {
-						if (writeError) {
-							reject(writeError);
-							return;
-						}
-						resolve();
-					});
+					console.log(JSON.stringify(jsonContent));
+					resolve({ language: targetLanguage, output: jsonContent });
+				})
+				.catch((translationError) => {
+					console.log(translationError);
 				});
 		});
 	}
