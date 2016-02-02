552898dc9245c495f563038c4989795fa5f155de
Lucas Vogel <contact@vogelino.ch>
2016-02-02 09:20:29 +0100

    Removed unused function

--- src/js/components/microphone/utils/records/index.js
+++ src/js/components/microphone/utils/records/index.js
@@ -6,34 +6,6 @@ import rec from 'node-record-lpcm16';
  */
 export default class Record {
 	/**
-	 * Records the audio and writes the input into a temp file
-	 * @param  {Object}   options  - The Recording options
-	 * @param  {Function} callback - The function to be called when over
-	 */
-	recordMaster(options, callback) {
-		const {
-			location,
-			filename,
-			id,
-			sampleRate,
-			temp,
-			master,
-			maxRecordingTime
-		} = options;
-
-		const file = fs.createWriteStream(
-			`${location}${temp}.wav`, {
-				encoding: 'binary'
-			});
-		rec.start({ sampleRate, verbose: true });
-
-		const timeout = setTimeout(() => {
-			rec.stop().pipe(file);
-			callback();
-			clearTimeout(timeout);
-		}, maxRecordingTime);
-	}
-	/**
 	 * Records the input in a wav file
 	 * @param  {Object} options - The recording options
 	 * @return {Promise}        - Is called when recorded and written in a file
